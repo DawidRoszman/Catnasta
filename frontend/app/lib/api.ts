06ed20bb3bfb449a9071964138386411bb6a6937
Dawid Roszman <dawidroszman@gmail.com>
2024-01-27 12:22:40 +0100

    for production

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,2 +1,2 @@
-// export const api = "https://catnasta.dawidroszman.eu";
-export const api = "http://localhost:5000";
+export const api = "https://catnasta.dawidroszman.eu";
+//export const api = "http://localhost:5000";
